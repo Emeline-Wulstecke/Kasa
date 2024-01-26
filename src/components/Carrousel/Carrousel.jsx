78e74790a9586f888b0b7cbe186dcff7c15ffaf5
emelinew1 <emeline.wulstecke@gmail.com>
2024-01-26 14:48:45 +0100

    creation of Carrousel

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -4,13 +4,37 @@ import arrowleft from '../../assets/images/arrowleft.png';
 import arrowright from '../../assets/images/arrowright.png';
 
 const Carrousel = ({ data }) => {
+    if (!data || !data.pictures || data.pictures.length === 0) {
+        return null; // Gestion des données manquantes ou incorrectes
+      }
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const previousImg = () => {
-        setCurrentImg((index) => (index === 0 ? data.pictures.length - 1 : index - 1))
-    };
-    const nextImg = () => {
-        setCurrentImg((index) => (index === data.pictures.length - 1 ? 0 : index + 1))
-    };
-}
-    
+  const previousImg = () => {
+    setCurrentIndex((index) => (index === 0 ? data.pictures.length - 1 : index - 1));
+  };
+
+  const nextImg = () => {
+    setCurrentIndex((index) => (index === data.pictures.length - 1 ? 0 : index + 1));
+  };
+
+  const currentImg = data.pictures[currentIndex];
+  const counterImg = data.pictures.length;
+
+  if (counterImg === 1) {
+    return <img src={currentImg} alt={data.title} className="carousel-img" />;
+  }
+
+  const displayCounter = `${currentIndex + 1}/${counterImg}`;
+
+  return (
+    <div>
+      <img src={currentImg} alt={data.title} className="carousel-img" />
+      <img src={arrowleft} alt="Flèche de gauche" className="arrow-left" onClick={previousImg} />
+      <img src={arrowright} alt="Flèche de droite" className="arrow-right" onClick={nextImg} />
+      <div className="carousel-counter">{displayCounter}</div>
+    </div>
+  );
+};
+
+export default Carrousel;
+
--- src/pages/Rental/Rental.jsx
+++ src/pages/Rental/Rental.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import './rental.scss'
+import Carrousel from '../../components/Carrousel/Carrousel';
+import data from '../../data/data.json'
 
 const Rental = () => {
   return (
     <main>
-
-
+      <Carrousel data={data} />
+       
     </main>
   )
 }
 
-export default Rental;
+export default Rental; 
